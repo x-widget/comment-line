001a246decb249da0607705c21ecc264507cd8ac
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-04-16 17:40:35 +0800

    fix for widget: comment-line

--- comment-line.php
+++ comment-line.php
@@ -4,10 +4,7 @@
 <div class='comment-line'>
 		<div class='title'><?=$widget_config['title']?></div>
 	<?
-		$latest_comments = g::posts(array( 'wr_is_comment' => '1', 'limit' => '4' ));
-			foreach ( $latest_comments as $comments ) {
-				$comments_list[] = db::row("SELECT wr_id, wr_content, wr_name, wr_email, wr_datetime, mb_id FROM $g5[write_prefix]".$comments['bo_table']." WHERE wr_id='".$comments['wr_id']."'");
-			}
+		$comments_list = g::posts(array( 'wr_is_comment' => '1', 'limit' => '4', 'select' => 'wr_content, wr_name', 'domain' => etc::domain() ));
 		$i = 1;
 		foreach ( $comments_list as $comment ) { ?>
 			<div class='latest-comments comment_<?=$i++?>'>
@@ -16,8 +13,7 @@
 					<span class='comment-author'>by <?=$comment['wr_name']?></span>
 				</div>
 			</div>
-		<? } ?>
-		
+		<? } ?>	
 		<div style='clear: left'></div>
 </div>
 <style>
