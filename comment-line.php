13f6f952dfc4632315cc0ec74edae65632710560
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-04-17 17:47:44 +0800

    Anchor Tag Fix

--- comment-line.php
+++ comment-line.php
@@ -5,15 +5,17 @@
 <div class='comment-line'>
 		<div class='title'><?=$widget_config['title']?></div>
 	<?
-		$comments_list = g::posts(array( 'wr_is_comment' => '1', 'limit' => '4', 'select' => 'wr_content, wr_name', 'domain' => etc::domain() ));
+		$comments_list = g::posts(array( 'wr_is_comment' => '1', 'limit' => '4', 'select' => 'wr_parent, wr_num,wr_is_comment, wr_id, bo_table, wr_content, wr_name', 'domain' => etc::domain() ));
 		$i = 1;
 		if( $comments_list ) { 
 			foreach ( $comments_list as $comment ) { ?>
+				<? $comment_url = g::url()."/bbs/board.php?bo_table=$comment[bo_table]&wr_id=$comment[wr_parent]&c_id=$comment[wr_id]&w=c#bo_vc_w" ?>
 				<div class='latest-comments comment_<?=$i++?>'>
 					<div class='inner'>
 						<span class='post-comment'><?=$comment['wr_content']?></span>
 						<span class='comment-author'>by <?=$comment['wr_name']?></span>
 					</div>
+					<a href="<?=$comment_url?>" class='read_comment'></a>
 				</div>
 			<? } ?>	
 			<div style='clear: left'></div>
